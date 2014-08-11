8ff88ab31f22beed07b93357ea7df5e2475bdf83
Michael Annan <micjr1@hotmail.com>
2014-08-11 02:35:15 +0000

    lab 4.1 - validating data

--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -10,4 +10,15 @@ class User < ActiveRecord::Base
     join_table: :follows,
     foreign_key: :followed_id,
     association_foreign_key: :follower_id
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 8}, if: :strong?
+
+  def strong?
+    password =~ /.*\d+.*/ && \
+    password =~ /.*[a-z]+.*/ && \
+    password =~ /.*[A-Z].*/
+  end
 end
