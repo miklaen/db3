c2c59bfe5c89e6ea25288e98b2f722b1fbc12310
Michael Annan <micjr1@hotmail.com>
2014-08-18 01:59:11 +0000

    Trying to fix followers issue

--- splatter/db/schema.rb
+++ splatter/db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20140805113800) do
 
   create_table "follows", force: true do |t|
     t.integer "follower_id"
-    t.integer "tollowed_id"
+    t.integer "followed_id"
   end
 
   create_table "splatts", force: true do |t|
