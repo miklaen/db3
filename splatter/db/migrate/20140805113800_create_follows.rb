448dd5c045d496fbaf3c095eb0c46e74998f7c87
Michael Annan <micjr1@hotmail.com>
2014-08-18 02:04:29 +0000

    Trying to fix followers issue

--- splatter/db/migrate/20140805113800_create_follows.rb
+++ splatter/db/migrate/20140805113800_create_follows.rb
@@ -2,7 +2,7 @@ class CreateFollows < ActiveRecord::Migration
   def change
     create_table :follows do |t|
       t.integer :follower_id, index: true
-      t.integer :tollowed_id, index: true
+      t.integer :followed_id, index: true
     end
   end
 end
