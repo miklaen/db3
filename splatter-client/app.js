701f8f6d0780aaa1598ed4bd16d7dfa2bceef654
Michael Annan <micjr1@hotmail.com>
2014-09-12 05:02:20 +0000

    milestone 2

--- splatter-client/app.js
+++ splatter-client/app.js
@@ -22,40 +22,31 @@
 		this.data = {};
 		this.updateUser = function(user) 
 		{			
-			//Set the user from the api
 			newUser = User.get({id: this.data.name});
-			//Set the news feed from the api
 			newFeed = SplattsFeed.get({id: this.data.name});
-			//Set the user and feed to be the user we just got
 			user.u = newUser;
 			user.feed = newFeed;
-			SplattsFeed.getfeed({id: this.data.id})
-			.$promise.then(function(value){user.feed.splatt = value});
 			this.data = {} // clear the form
 		}
 	});	
 		
 	app.controller("ShowSplattsController", function(User, SplattsFeed) {
 		this.data = {};
-		this.updateFeed = function(user) 
-		{			
+		this.updateFeed = function(user) {			
 			SplattsFeed.getfeed({id: this.data.id})
 			.$promise.then(function(value){user.feed.splatt = value});
 			this.data = {} // clear the form		
 		}
-		this.updateSplatts = function(user) 
-		{			
+		this.updateSplatts = function(user) {			
 			SplattsFeed.getsplatts({id: this.data.id})
 			.$promise.then(function(value){user.feed.splatt = value});
 			this.data = {} // clear the form		
 		}
 	});	
 	
-	app.controller("DeleteUserController", function(User) 
-	{
+	app.controller("DeleteUserController", function(User) {
 		this.data = {};
-		this.deleteUser = function(user) 
-		{	
+		this.deleteUser = function(user) {	
 			deleteId = this.data.id;			
 			User.delete({id: deleteId});
 			this.data = {} // clear the form
@@ -64,38 +55,35 @@
 
 	app.controller("UpdateUserController", function(User) {	
 		this.data = {};
-		this.updateUser = function(user) 
-		{	
-			postData = {"user":{
+		this.updateUser = function(user) {	
+			newUser = {"user":{
 				"name": this.data.name, 
   				"blurb": this.data.blurb 
-			}}
-			
-				
-			User.update({id: user.u.id},postData);
-			this.data = {} //clears the form
+			}}			
+			User.update({id: user.u.id},newUser);
+			this.data = {} // clear the form
 		}
 	});
 	
+	//something may be wrong with this controller
 	app.controller("AddSplattController", function(User) {	
 		this.data = {};
 		this.addSplatt = function(user) {	
-			postData = {"splatt":{
+			newSplatt = {"splatt":{
 				"body": this.data.splatt, 
   				"user_id": user.u.id 
-			}}
-					
-			User.addsplatt({},postData);
-			this.data = {} //clears the form
+			}}		
+			User.addsplatt({},newSplatt);
+			this.data = {} // clear the form
 		}
 	});
 	
-		app.controller("FollowUserController", function(User) {	
+	app.controller("FollowUserController", function(User) {	
 		this.data = {};
 		this.followUser = function(user) {	
 			followUserData = {"id": user.u.id,"follows_id": this.data.follows}
 			User.follow({},followUserData);
-			this.data = {} //clears the form
+			this.data = {} // clear the form
 		}
 	});
 	
@@ -103,23 +91,20 @@
 		this.data = {};
 		this.unfollowUser = function(user) {	
 			User.unfollow({id: user.u.id,follows_id: this.data.follows},{});
-			this.data = {} //clears the form
+			this.data = {} // clear the form
 		}
 	});
 		
-	app.controller("NewUserFormController", function(User, $http) {
+	app.controller("AddUserController", function(User, $http) {
 		this.data = {};
-		this.newUser = function(user) 
-		{	
-			postData = {"user":{
+		this.newUser = function(user) {	
+			newUser = {"user":{
   				"email": this.data.email, 
   				"name": this.data.name, 
 				"password": this.data.password,
   				"blurb": this.data.blurb 
-			}}
-			
-			
-			 User.save(postData);
+			}}		
+			User.save(newUser);
 			this.data = {} // clear the form
 		}
 	});	
