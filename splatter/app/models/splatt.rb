9ecb3ca5e541bdf015471d9ec81e8143819abd87
Michael Annan <micjr1@hotmail.com>
2014-10-21 21:56:06 +0000

    added splatt_repo

--- splatter/app/models/splatt.rb
+++ splatter/app/models/splatt.rb
@@ -1,6 +1,5 @@
-class Splatt < ActiveRecord::Base
-  belongs_to :user
-
-  validates :body, presence: true
-  validates :belongs_to, presence: true
+class Splatt < Hashie::Dash
+  property :id
+  property :body
+  property :created_at
 end
